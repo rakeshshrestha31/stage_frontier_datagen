694c0e0b63547ca1888d9e2b55206876336006c2
rakeshs <rakesh_shrestha@sfu.ca>
2018-06-12 15:20:58 -0700

    cartographer does mapping, but costmap isn't generated because the size of the map changes every time

--- config/cartographer.lua
+++ config/cartographer.lua
@@ -21,10 +21,10 @@ options = {
   map_frame = "map",
   tracking_frame = "base_link",
   published_frame = "odom",
-  odom_frame = "odom",
-  provide_odom_frame = false,
+   odom_frame = "odom",
+   provide_odom_frame = false,
   publish_frame_projected_to_2d = false,
-  use_odometry = false,
+  use_odometry = true,
   use_nav_sat = false,
   use_landmarks = false,
   num_laser_scans = 1,
@@ -32,9 +32,9 @@ options = {
   num_subdivisions_per_laser_scan = 1,
   num_point_clouds = 0,
   lookup_transform_timeout_sec = 0.2,
-  submap_publish_period_sec = 1.0, -- 0.3,
-  pose_publish_period_sec = 0.1, -- 5e-3,
-  trajectory_publish_period_sec = 0.1, -- 30e-3,
+  submap_publish_period_sec = 0.3,
+  pose_publish_period_sec = 5e-3,
+  trajectory_publish_period_sec = 30e-3,
   rangefinder_sampling_ratio = 1.,
   odometry_sampling_ratio = 1.,
   fixed_frame_pose_sampling_ratio = 1.,
@@ -43,7 +43,11 @@ options = {
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
-TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 10
+
 TRAJECTORY_BUILDER_2D.use_imu_data = false
+TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
+TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
+TRAJECTORY_BUILDER_2D.max_range = 8
+
 
 return options
